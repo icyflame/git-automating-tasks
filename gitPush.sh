ed6451a6083d63eeacdb88d0e38e77a2e9181ef3
IcyFlame <kannan.siddharth12@gmail.com>
2013-05-23 21:09:08 +0530

    isolated the commit and push operations

--- gitPush.sh
+++ gitPush.sh
@@ -1,30 +1,6 @@
 #!/bin/bash
 
-echo 'current status:'
-echo '=================================='
-echo '=================================='
-
-git status
-
-echo 'adding all the files in the folder'
-
-git add .
-
-echo '=================================='
-echo '=================================='
-
-echo "Enter the commit message:"
-read commitMes
-
-git commit -m "$commitMes"
-
-echo '=================================='
-echo '=================================='
-echo 'the changes have been comitted:'
-echo '=================================='
-echo '=================================='
-git log
-
+#will try to push all the changes made till now to the server.
 
 echo
 echo 'enter the name of the branch:'
@@ -33,7 +9,6 @@ read branch
 echo 'trying to push data to server:'
 echo '=================================='
 echo '=================================='
-
 
 git push origin $branch
 
