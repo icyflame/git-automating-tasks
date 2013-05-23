6b7c65c00be85b9fce4ef3c9d3e5742a69a24c30
IcyFlame <kannan.siddharth12@gmail.com>
2013-05-23 13:15:17 +0530

    modified gitInit.sh so as to make it more userfriendly.

--- gitInit.sh
+++ gitInit.sh
@@ -1,14 +1,36 @@
 #!/bin/bash
 
+#this will initialise(or reinitialise) a git repository and add the readme file.
+
+echo 'initialising a git repository:'
+echo
 git init
 
+echo 'created the readme file:'
 touch readme
 
-echo 'this is the readme file' > readme
+echo 
+echo 'enter the contents of the readme file:'
+read contentsOfReadme
+
+echo "$contentsOfReadme" > readme
 
 git add .
 
+echo
+echo
+echo 'all the files added to repo'
+echo '=================================='
+echo '=================================='
+echo 'current status:'
+echo
+echo
+echo
+
 git status
+
+echo '=================================='
+echo '=================================='
 
 echo "Enter the commit message:"
 read commitMes
@@ -18,7 +40,8 @@ git commit -m "$commitMes"
 echo 'The log for this git repo is as so:'
 echo
 echo
-echo "----------------"
+echo '=================================='
+echo '=================================='
 git log
 
 read
