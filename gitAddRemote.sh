ce95feeff4e1470338ae380767469f8ae2e47c74
IcyFlame <kannan.siddharth12@gmail.com>
2013-05-23 13:33:30 +0530

    pushing to server

--- gitAddRemote.sh
+++ gitAddRemote.sh
@@ -2,3 +2,13 @@
 
 #this script will take the remote address from the user and
 #connect the git repository to the server.
+
+echo 'enter the url of the repository:'
+read url
+
+git remote add origin $url
+
+echo 'push destination has been locked'
+echo "run \'gitPush.sh\' to push everything to the server"
+
+read
